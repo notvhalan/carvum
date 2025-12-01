d3f7f733f46f751c34336dd34ad3b6e02694f02a
Vhalan <vhalan02@gmail.com>
2025-12-01 21:02:40 +0000

    sd

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -23,31 +23,16 @@ const Navbar = () => {
           </ul>
         </li>
         <li>
-          Services <span className="dropdown-arrow">▼</span> {/* Added className */}
+          Policies <span className="dropdown-arrow">▼</span>
           <ul className="dropdown">
             <li>
-              <a href="https://www.instagram.com/amusementsgroundsstagnating" target="_blank" rel="noopener noreferrer">
-                Instagram
+              <a href="/privacyc">
+                CardCounter Privacy Policy
               </a>
             </li>
             <li>
-              <a href="https://www.youtube.com/@vhalan" target="_blank" rel="noopener noreferrer">
-                YouTube
-              </a>
-            </li>
-            <li>
-              <a href="https://www.linkedin.com/in/vhalaniscool?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app" target="_blank" rel="noopener noreferrer">
-                LinkedIn
-              </a>
-            </li>
-            <li>
-              <a href="https://github.com/notvhalan" target="_blank" rel="noopener noreferrer">
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="http://carvum.life" target="_blank" rel="noopener noreferrer">
-                Personal Website
+              <a href="/empathticspolicy">
+                Empathytics Privacy Policy
               </a>
             </li>
           </ul>
