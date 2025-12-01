5aa12de128eb83cb11d3a19cb7eed70c3f973219
Vhalan <vhalan02@gmail.com>
2025-12-01 21:29:23 +0000

    Add social media links and personal website to Services dropdown in Navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -23,6 +23,36 @@ const Navbar = () => {
           </ul>
         </li>
         <li>
+          Services <span className="dropdown-arrow">▼</span> {/* Added className */}
+          <ul className="dropdown">
+            <li>
+              <a href="https://www.instagram.com/amusementsgroundsstagnating" target="_blank" rel="noopener noreferrer">
+                Instagram
+              </a>
+            </li>
+            <li>
+              <a href="https://www.youtube.com/@vhalan" target="_blank" rel="noopener noreferrer">
+                YouTube
+              </a>
+            </li>
+            <li>
+              <a href="https://www.linkedin.com/in/vhalaniscool?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app" target="_blank" rel="noopener noreferrer">
+                LinkedIn
+              </a>
+            </li>
+            <li>
+              <a href="https://github.com/notvhalan" target="_blank" rel="noopener noreferrer">
+                GitHub
+              </a>
+            </li>
+            <li>
+              <a href="http://carvum.life" target="_blank" rel="noopener noreferrer">
+                Personal Website
+              </a>
+            </li>
+          </ul>
+        </li>
+        <li>
           Policies <span className="dropdown-arrow">▼</span>
           <ul className="dropdown">
             <li>
@@ -37,7 +67,6 @@ const Navbar = () => {
             </li>
           </ul>
         </li>
-        <li>Cases</li>
         <li>Contact</li>
       </ul>
     </nav>
